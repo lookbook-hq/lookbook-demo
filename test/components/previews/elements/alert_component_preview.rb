b0283ce3d131eb5bb027b7745c59d214cf6089da
Mark Perkins <mark@allmarkedup.com>
2021-07-13 21:30:49 +0100

    Add alert component previews

--- test/components/previews/elements/alert_component_preview.rb
+++ test/components/previews/elements/alert_component_preview.rb
@@ -2,7 +2,7 @@
 class Elements::AlertComponentPreview < ViewComponent::Preview
   def warn
     render Elements::AlertComponent.new(message: "Something went wrong :-(", style: :warn)
-  end
+  end 
 
   def success
     render Elements::AlertComponent.new(message: "That was successful!", style: :success)
