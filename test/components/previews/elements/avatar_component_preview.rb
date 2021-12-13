70b236f27805e17e6c0dfb45c84dc480b922bf8d
Dale Morgan <dale.morgan@freeagent.com>
2021-12-13 14:43:18 +0000

    Demonstrating that background colour requires quotes in Avatar

--- test/components/previews/elements/avatar_component_preview.rb
+++ test/components/previews/elements/avatar_component_preview.rb
@@ -1,6 +1,7 @@
 class Elements::AvatarComponentPreview < ViewComponent::Preview
   # @label Rounded
   #
+  # @display bg_color #f00
   # @param size [Symbol] select [[Small, sm], [Medium, md], [Large, lg]]]
   def default(size: :md)
     render Elements::AvatarComponent.new(src: "https://placekitten.com/300/300", size: size)
@@ -8,6 +9,7 @@ class Elements::AvatarComponentPreview < ViewComponent::Preview
 
   # @label Square
   #
+  # @display bg_color "#f00"
   # @param (see #default)
   def format_square(size: :md)
     render Elements::AvatarComponent.new(src: "https://placekitten.com/300/300", size: size, square: true)
