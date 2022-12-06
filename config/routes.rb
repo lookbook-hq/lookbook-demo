9c6dc71a57a64e195cbde9646e2e618db88dbd57
Mark Perkins <mark@allmarkedup.com>
2022-12-06 16:23:50 +0000

    More cleanup and improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "demo#index"
-  get "embeds", to: "demo#embeds"
+  get "embeds", to: "demo#embeds", as: :embeds
 
   mount Lookbook::Engine, at: "/lookbook"
 end
