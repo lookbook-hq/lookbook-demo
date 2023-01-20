1a1f4cdecad91ff2d94c8ce1cc049b926630aaaf
Mark Perkins <mark@allmarkedup.com>
2023-01-20 13:49:42 +0000

    Use `with_` prefix for slots

--- test/components/previews/elements/list_component_preview.rb
+++ test/components/previews/elements/list_component_preview.rb
@@ -5,9 +5,9 @@ class Elements::ListComponentPreview < ViewComponent::Preview
   # You can read more about it in the ViewComponent [slots documentation](https://viewcomponent.org/guide/slots.html#with_content).
   def default
     render Elements::ListComponent.new do |list|
-      list.item.with_content "One"
-      list.item.with_content "Two"
-      list.item.with_content "Three"
+      list.with_item.with_content "One"
+      list.with_item.with_content "Two"
+      list.with_item.with_content "Three"
     end
   end
 end
