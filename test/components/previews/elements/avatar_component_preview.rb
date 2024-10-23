5cd86331a8ddbcad00d74e9808576c0ca148cabd
Mark Perkins <mark@allmarkedup.com>
2024-10-23 21:44:37 +0100

    Fix broken placeholder image URLs

--- test/components/previews/elements/avatar_component_preview.rb
+++ test/components/previews/elements/avatar_component_preview.rb
@@ -2,13 +2,13 @@ class Elements::AvatarComponentPreview < ViewComponent::Preview
   # @label Rounded
   # @param size [Symbol] select "The size to display the avatar at." { choices: [[Small, sm], [Medium, md], [Large, lg]] }
   def default(size: :md)
-    render Elements::AvatarComponent.new(src: "https://placekitten.com/300/300", size: size)
+    render Elements::AvatarComponent.new(src: "https://loremflickr.com/300/300", size: size)
   end
 
   # @label Square
   #
   # @param (see #default)
   def format_square(size: :md)
-    render Elements::AvatarComponent.new(src: "https://placekitten.com/300/300", size: size, square: true)
+    render Elements::AvatarComponent.new(src: "https://loremflickr.com/300/300", size: size, square: true)
   end
 end
