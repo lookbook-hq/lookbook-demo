caffc1c7d6abbfa8b4660236d6899e579ee6ac98
Mark Perkins <mark@allmarkedup.com>
2025-03-01 11:22:57 +0000

    Fix Phlex test component

--- app/views/phlex/example.rb
+++ app/views/phlex/example.rb
@@ -4,7 +4,7 @@ module Views::Phlex
 			@emoji = emoji
 		end
 
-		def template
+		def view_template
 			div class: "prose text-center mx-auto p-8 rounded-lg bg-green-50 border-dashed border-2 border-green-400" do
 				h3(class: "text-xl font-bold mb-4") { "Phlex example #{@emoji}" }
 				p { "This is a Phlex component being rendered by Lookbook." }
